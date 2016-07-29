b6cc214a516f5e6b423e266695a135089ac9d04e
Nick Charchut <nicholas.charchut@gmail.com>
2016-07-29 02:43:19 -0400

    added dropdown menu

---
+++ js/drop_down_menus.js
@@ -0,0 +1,35 @@
+var test = (function() {
+	var init, lists, _Drop;
+
+	_Drop = Drop.createContext({
+		classPrefix: 'drop'
+	});
+
+	init = function() {
+		return setupMenus();
+	};
+
+	setupMenus = function() {
+		return $('.dropdown').each(function() {
+			var $dropdown, $target, content, drop, openOn, theme;
+			$dropdown = $(this);
+			theme = $dropdown.data('theme');
+			openOn = $dropdown.data('open-on') || 'click';
+			$target = $dropdown.find('.drop-target');
+			$target.addClass(theme);
+			content = $dropdown.find('.drop-content').html();
+			return drop = new _Drop({
+				target: $target[0],
+				classes: theme,
+				position: 'bottom center',
+				constrainToWindow: true,
+				constraintoScrollParent: false,
+				openOn: openOn,
+				content: content
+			});
+		});
+	};
+
+	init();
+
+}).call(this)
---
+++ js/dumb.js
@@ -0,0 +1,8 @@
+var drop;
+
+drop = new Drop({
+  target: document.querySelector('.drop-target'),
+  content: 'Welcome to the future!',
+  position: 'bottom left',
+  openOn: 'click'
+});
